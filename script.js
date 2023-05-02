611ccf4754986d19aa9df9691cc56c2b3120c38c
shivani sawarkar <117519427+shivanisawarakar@users.noreply.github.com>
2023-05-02 15:54:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function insert_Row() {
-    //Write your code here
-  
+//get the table by its ID
+
+	var table = Document.getElementById("sampleTable");
+
+	//Insert a new row at the top of the table 
+	var newRow = table.insertRow(0);
+
+	//Insert a new cell in the new row 
+	var leftCell = newRoll.insertCell(0);
+	ver rightCell = newRoll.insertCell(1);
+
+	//Set the value of the left cell to the "New Cell" and the right cell to "New Cell2"
+    leftCell.innerHTML = "New Cell";
+	rightCell. innerHTML = "New Cell2";
   
 }
